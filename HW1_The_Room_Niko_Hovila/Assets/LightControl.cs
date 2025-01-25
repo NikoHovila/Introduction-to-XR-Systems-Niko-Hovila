548a26329a02f8f6271eae51521f5d360eb71d09
NikoHovila <niko.hovila22@gmail.com>
2025-01-25 11:29:08 +0200

    Some fixes
    Some fixes like Skybox, light switch, README and blender windowed wall added

--- HW1_The_Room_Niko_Hovila/Assets/LightControl.cs
+++ HW1_The_Room_Niko_Hovila/Assets/LightControl.cs
@@ -6,14 +6,16 @@ public class LightControl : MonoBehaviour
     public InputActionReference action;
     public Light light;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool isRed = false;
+
     void Start()
     {
         light = GetComponent<Light>();
         action.action.Enable();
         action.action.performed += ctx =>
         {
-            light.enabled = !light.enabled;
-            light.color = Color.red;
+            isRed = !isRed;
+            light.color = isRed ? Color.red : Color.white; // Alternate between red and white
         };
     }
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Introduction to XR Systems Niko Hovila
+# GitHub
+# https://github.com/NikoHovila/Introduction-to-XR-Systems-Niko-Hovila
+
+# HW1 "A More Interesting Room"
+# -Curved roof
+# -Window
  
